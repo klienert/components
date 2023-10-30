badb5d618ee11ea939a5c5e888b74b2706cd1f7b
Keith <klienert@madisoncollege.edu>
2023-10-30 17:55:31 -0500

    some additional calculations for the calculator

--- src/app/kl-mortgage-calc/kl-mortgage-calc.component.ts
+++ src/app/kl-mortgage-calc/kl-mortgage-calc.component.ts
@@ -14,17 +14,37 @@ export class KlMortgageCalcComponent implements OnInit {
     // inputs
     loanAmount = 245000;
     loanTerm = 30;
-    interestRate = 7.5;    
-
+    interestRate = 7.5;
+    localTax = 0;
+    hoi = 0;
+    hoa = 0;
+    
     /**         
      * @returns monthly payment based on the above inputs
      */
     calculate = () => {      
       const monthlyInterest = (this.interestRate * 0.01) / 12; // changes percentage to decimal -> / 12
       const numPayments = this.loanTerm * 12;
-      const payment = 
+      let payment = 
           (this.loanAmount * monthlyInterest) / ( 1 - Math.pow(1 + monthlyInterest, -numPayments));
+      
+      // conditionals since these will be optional
+      if (this.localTax > 0) {
+        payment += payment * (this.localTax * 0.01);
+      }
+      
+      if (this.hoi > 0) {
+        payment = payment + this.hoi;
+      }
+
+      if (this.hoa > 0) {
+        payment = payment + this.hoa;
+      }
+
       return payment;
+
     }   
+
+    
 
 }
